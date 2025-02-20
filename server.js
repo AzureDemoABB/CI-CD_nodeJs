7dcd7ea7ab69ea514a130f1b7bac51a8b262db31
Raghunandan <Raghunandan.Pattar@dominos.co.uk>
2025-02-21 01:51:33 +0530

    made some changes to the application

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-
 const express = require("express");
-const app = express();
+const path = require("path");
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from 'public' folder
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("Hello, Azure!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log('Server is running on port->' +PORT);
+  console.log(`Server is running on port ${PORT}`);
 });
-
